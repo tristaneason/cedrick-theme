18c75558e9022685dd1c398acf102a664e3627b6
Nolan <nolanperk@gmail.com>
2017-01-13 15:07:05 -0500

    Changed allInits to have page inits

--- js/scripts/interactive.js
+++ js/scripts/interactive.js
@@ -1,11 +1,13 @@
 // DOM Ready
 (function($, window, undefined) {
    $(function() {
+      var interactiveInit = function() {
 
-      // =================================================================
-         //All menu opening transitions and other interactive elements should go here!
-      // =================================================================
+         // =================================================================
+            //All menu opening transitions and other interactive elements should go here!
+         // =================================================================
 
-
+      };
+      interactiveInit();
    });
 })(jQuery, window);
