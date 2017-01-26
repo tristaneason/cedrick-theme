2ddd66bcf8758827d62958cec3b0c4b0214cb7b4
Josh Carey <josh@harbr.co>
2017-01-26 16:17:48 -0500

    update comment in global

--- js/scripts/global.js
+++ js/scripts/global.js
@@ -4,7 +4,7 @@
       var globalsInit = function() {
 
          // =================================================================
-            //Put all styles that are to be fired on load here. If there becomes too many page specific styles, then expand it to a "_page.js" file.
+            //Put all scripts that are to be fired on load here. If there becomes too many page specific scripts, then expand it to a "_page.js" file.
          // =================================================================
 
       };
