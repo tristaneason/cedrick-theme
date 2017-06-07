10b78415a264d7f4c7823f9c8bd8dc8bcb481dbf
Tristan Eason <tristaneason@protonmail.com>
2017-06-07 10:46:56 -0400

    Included Title, Date, and Tags in to post.

--- wp-content/themes/cedrick/single-project.php
+++ wp-content/themes/cedrick/single-project.php
@@ -8,7 +8,13 @@ get_header();
 if(have_posts()) {
 	while(have_posts()) {
 		the_post();
-		
+		the_title();
+		the_date("Y");
+		$tags = wp_get_post_tags($post->ID);
+		foreach($tags as $object => $tag) {
+			$tag_name = $tag->name . ", ";
+			echo $tag_name;
+		}
 	}
 }
 
